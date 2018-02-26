dfdb5a4433b91354e399ea09be3c05b11bc24685
WanliMa <wxm146@case.edu>
2018-02-26 14:04:16 -0500

    add server

--- tap-news/client/src/index.js
+++ tap-news/client/src/index.js
@@ -1,7 +1,6 @@
+import App from './App/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
