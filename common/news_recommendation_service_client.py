24abbc8cc387410befd821e3da966901f5f91f82
WanliMa <wxm146@case.edu>
2018-03-23 00:25:59 -0400

    fix pyjsonrpc problem

--- common/news_recommendation_service_client.py
+++ common/news_recommendation_service_client.py
@@ -1,10 +1,10 @@
-import pyjsonrpc
+import jsonrpclib
 
 URL = "http://localhost:5050/"
 
-client = pyjsonrpc.HttpClient(url=URL)
+client = jsonrpclib.ServerProxy(URL)
 
 def getPreferenceForUser(userId):
-    preference = client.call("getPreferenceForUser", userId)
+    preference = client.getPreferenceForUser(userId)
     print("Preference list: %s" % str(preference))
     return preference
